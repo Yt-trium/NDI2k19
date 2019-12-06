8e79bdcf75e883d131a670d967def29d1738549a
Kevin Masson <hi@oktomus.com>
2019-12-06 03:30:44 +0100

    Integrate profile

--- js/ours.js
+++ js/ours.js
@@ -22,7 +22,7 @@ const school_quality = ["Très mal", "Mal", "Bien", "Très bien"];
 const social_quality = ["Très mal", "Mal", "Bien", "Très bien"];
 const canvas = document.getElementById('output');
 
-let net;
+let grouped_stuff;
 
 let data = {
 age : 23,
@@ -141,6 +141,13 @@ if (showVideo)
     ctx.restore();
 }
 
+const net = await posenet.load({
+    architecture: 'MobileNetV1',
+    outputStride: 16,
+    inputResolution: { width: videoWidth, height: videoHeight },
+    multiplier: 1.0
+});
+
 async function frame() {
     const pose = await net.estimatePoses(video, {
         flipHorizontal: true,
@@ -232,32 +239,38 @@ phase8to9.onclick = function() {
 animateCSS(phase8, "fadeOutLeft", function() {
     phase8.style.display = "none";
     phase9.style.display = "block";
+    grouped_stuff.position.y = 0.3;
     animateCSS(phase9, "fadeInRight");
 })
 }
 
 plusSocial.onclick = function() {
 data.social = Math.min(3, data.social + 1);
+update_profile();
 social.innerHTML = social_quality[data.social];
 }
 
 minusSocial.onclick = function() {
 data.social = Math.max(0, data.social - 1);
+update_profile();
 social.innerHTML = social_quality[data.social];
 }
 
 plusSchool.onclick = function() {
 data.school = Math.min(3, data.school + 1);
+update_profile();
 school.innerHTML = school_quality[data.school];
 }
 
 minusSchool.onclick = function() {
 data.school = Math.max(0, data.school - 1);
+update_profile();
 school.innerHTML = school_quality[data.school];
 }
 
 plusRevenus.onclick = function() {
 data.revenus = parseInt(revenus.innerHTML) + 100;
+update_profile();
 revenus.innerHTML = data.revenus;
 revenus.classList.remove("animated", "pulse")
 revenus.classList.add("animated", "pulse")
@@ -265,6 +278,7 @@ revenus.classList.add("animated", "pulse")
 
 minusRevenus.onclick = function() {
 data.revenus = Math.max(0, parseInt(revenus.innerHTML) - 100);
+update_profile();
 revenus.innerHTML = data.revenus;
 revenus.classList.remove("animated", "pulse")
 revenus.classList.add("animated", "pulse")
@@ -272,6 +286,7 @@ revenus.classList.add("animated", "pulse")
 
 plusLoyer.onclick = function() {
 data.loyer = parseInt(loyer.innerHTML) + 100;
+update_profile();
 loyer.innerHTML = data.loyer;
 loyer.classList.remove("animated", "pulse")
 loyer.classList.add("animated", "pulse")
@@ -279,6 +294,7 @@ loyer.classList.add("animated", "pulse")
 
 minusLoyer.onclick = function() {
 data.loyer = Math.max(0, parseInt(loyer.innerHTML) - 100);
+update_profile();
 loyer.innerHTML = data.loyer;
 loyer.classList.remove("animated", "pulse")
 loyer.classList.add("animated", "pulse")
@@ -286,16 +302,19 @@ loyer.classList.add("animated", "pulse")
 
 plusDistance.onclick = function() {
 data.distance = parseInt(distance.innerHTML) + 5;
+update_profile();
 distance.innerHTML = data.distance;
 }
 
 minusDistance.onclick = function() {
 data.distance = Math.max(0, parseInt(distance.innerHTML) - 5);
+update_profile();
 distance.innerHTML = data.distance;
 }
 
 plusAge.onclick = function() {
 data.age = parseInt(age.innerHTML) + 1;
+update_profile();
 age.innerHTML = data.age;
 age.classList.remove("animated", "pulse")
 age.classList.add("animated", "pulse")
@@ -303,6 +322,7 @@ age.classList.add("animated", "pulse")
 
 minusAge.onclick = function() {
 data.age = Math.max(0, parseInt(age.innerHTML) - 1);
+update_profile();
 age.innerHTML = data.age;
 age.classList.remove("animated", "pulse")
 age.classList.add("animated", "pulse")
@@ -342,6 +362,8 @@ scene.add(ambientLight);
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add(directionalLight);
 
+// Visage 3D
+
 var loader = new THREE.OBJLoader();
 let mask;
 loader.load(
@@ -361,17 +383,158 @@ function ( error ) {
 }
 );
 
+// Profil de la personne
+
+let vertices = [
+new THREE.Vector3(0.0, 0.0, 0.0),
+new THREE.Vector3(0.0, 0.5, 0.0),
+new THREE.Vector3(-0.43, 0.25, 0.0),
+new THREE.Vector3(-0.43, -0.25, 0.0),
+new THREE.Vector3(0.0, -0.5, 0.0),
+new THREE.Vector3(0.43, -0.25, 0.0),
+new THREE.Vector3(0.43, 0.25, 0.0)
+];
+
+let lines_geometry = new THREE.Geometry();
+lines_geometry.vertices.push(vertices[0], vertices[1]);
+lines_geometry.vertices.push(vertices[0], vertices[2]);
+lines_geometry.vertices.push(vertices[0], vertices[3]);
+lines_geometry.vertices.push(vertices[0], vertices[4]);
+lines_geometry.vertices.push(vertices[0], vertices[5]);
+lines_geometry.vertices.push(vertices[0], vertices[6]);
+lines_geometry.vertices.push(vertices[1], vertices[2]);
+lines_geometry.vertices.push(vertices[2], vertices[3]);
+lines_geometry.vertices.push(vertices[3], vertices[4]);
+lines_geometry.vertices.push(vertices[4], vertices[5]);
+lines_geometry.vertices.push(vertices[5], vertices[6]);
+lines_geometry.vertices.push(vertices[6], vertices[1]);
+
+let hexa_edges0 = new THREE.LineSegments(lines_geometry, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+let hexa_edges1 = new THREE.LineSegments(lines_geometry, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+hexa_edges1.scale.set(0.75, 0.75, 0.75);
+let hexa_edges2 = new THREE.LineSegments(lines_geometry, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+hexa_edges2.scale.set(0.5, 0.5, 0.5);
+let hexa_edges3 = new THREE.LineSegments(lines_geometry, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+hexa_edges3.scale.set(0.25, 0.25, 0.25);
+
+let var1_mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.025, 16, 16 ), new THREE.MeshLambertMaterial({color: 0xFF0000}));
+let var2_mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.025, 16, 16 ), new THREE.MeshLambertMaterial({color: 0xFFFF00}));
+let var3_mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.025, 16, 16 ), new THREE.MeshLambertMaterial({color: 0x00FF00}));
+let var4_mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.025, 16, 16 ), new THREE.MeshLambertMaterial({color: 0x00FFFF}));
+let var5_mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.025, 16, 16 ), new THREE.MeshLambertMaterial({color: 0x0000FF}));
+let var6_mesh = new THREE.Mesh(new THREE.SphereGeometry( 0.025, 16, 16 ), new THREE.MeshLambertMaterial({color: 0xFF00FF}));
+
+let variable_positions = [
+    new THREE.Vector3(),
+    new THREE.Vector3(),
+    new THREE.Vector3(),
+    new THREE.Vector3(),
+    new THREE.Vector3(),
+    new THREE.Vector3(),
+    new THREE.Vector3()];
+     
+variable_positions[0].copy(vertices[0]);
+variable_positions[1].copy(vertices[1]);
+variable_positions[2].copy(vertices[2]);
+variable_positions[3].copy(vertices[3]);
+variable_positions[4].copy(vertices[4]);
+variable_positions[5].copy(vertices[5]);
+variable_positions[6].copy(vertices[6]);
+
+let variable_colors = [new THREE.Color(0xDDDDDD),new THREE.Color(0xFF0000),new THREE.Color(0xFFFF00),new THREE.Color(0x00FF00),new THREE.Color(0x00FFFF),new THREE.Color(0x0000FF),new THREE.Color(0xFF00FF)];
+
+let var_geometry = new THREE.Geometry();
+var_geometry.vertices = variable_positions;
+
+let face0 = new THREE.Face3(0, 1, 2);
+face0.vertexColors[0] = variable_colors[0];
+face0.vertexColors[1] = variable_colors[1];
+face0.vertexColors[2] = variable_colors[2];
+let face1 = new THREE.Face3(0, 2, 3);
+face1.vertexColors[0] = variable_colors[0];
+face1.vertexColors[1] = variable_colors[2];
+face1.vertexColors[2] = variable_colors[3];
+let face2 = new THREE.Face3(0, 3, 4);
+face2.vertexColors[0] = variable_colors[0];
+face2.vertexColors[1] = variable_colors[3];
+face2.vertexColors[2] = variable_colors[4];
+let face3 = new THREE.Face3(0, 4, 5);
+face3.vertexColors[0] = variable_colors[0];
+face3.vertexColors[1] = variable_colors[4];
+face3.vertexColors[2] = variable_colors[5];
+let face4 = new THREE.Face3(0, 5, 6);
+face4.vertexColors[0] = variable_colors[0];
+face4.vertexColors[1] = variable_colors[5];
+face4.vertexColors[2] = variable_colors[6];
+let face5 = new THREE.Face3(0, 6, 1);
+face5.vertexColors[0] = variable_colors[0];
+face5.vertexColors[1] = variable_colors[6];
+face5.vertexColors[2] = variable_colors[1];
+
+var_geometry.faces.push(face0);
+var_geometry.faces.push(face1);
+var_geometry.faces.push(face2);
+var_geometry.faces.push(face3);
+var_geometry.faces.push(face4);
+var_geometry.faces.push(face5);
+
+let var_mesh = new THREE.Mesh(var_geometry, new THREE.MeshLambertMaterial({vertexColors: THREE.VertexColors, transparent: true, opacity: 0.7}));
+var_mesh.position.z += 0.01;
+
+grouped_stuff = new THREE.Group();
+grouped_stuff.add(hexa_edges0, hexa_edges1, hexa_edges2, hexa_edges3,var1_mesh, var2_mesh, var3_mesh, var4_mesh, var5_mesh, var6_mesh, var_mesh);
+scene.add(grouped_stuff);
+grouped_stuff.position.set(-0.5, 0, 0);
+
+function set_var_positions(a, b, c, d, e, f)
+{
+    console.log("Set var positions: ", a, b, c, d, e, f);
+    variable_positions[1].copy(vertices[1]).multiplyScalar(a);
+    variable_positions[2].copy(vertices[2]).multiplyScalar(b);
+    variable_positions[3].copy(vertices[3]).multiplyScalar(c);
+    variable_positions[4].copy(vertices[4]).multiplyScalar(d);
+    variable_positions[5].copy(vertices[5]).multiplyScalar(e);
+    variable_positions[6].copy(vertices[6]).multiplyScalar(f);
+    var1_mesh.position.copy(variable_positions[1]);
+    var2_mesh.position.copy(variable_positions[2]);
+    var3_mesh.position.copy(variable_positions[3]);
+    var4_mesh.position.copy(variable_positions[4]);
+    var5_mesh.position.copy(variable_positions[5]);
+    var6_mesh.position.copy(variable_positions[6]);
+    var_mesh.geometry.verticesNeedUpdate = true;
+}
+
+set_var_positions(0, 0, 0, 0, 0, 0);
+
+// Map a value f in [0, 1]
+function map(f, min, max)
+{
+    output = (f - min) / (max - min)
+    output = Math.min(1.0, Math.max(0.0, output));
+    return output;
+}
+
+function update_profile()
+{
+    set_var_positions(
+        map(data.age, 16.0, 26.0),
+        map(data.revenus, 0.0, 1000.0),
+        map(data.loyer, 0.0, 1000.0),
+        map(data.distance, 0.0, 30.0),
+        map(data.school, 0.0, 3.0),
+        map(data.social, 0.0, 3.0));
+}
 
 function render()
 {
-renderer.render( scene, camera );
-
+    renderer.render( scene, camera );
 }
 
 function startAnimation() {
-animate();
-renderer.domElement.style.display = "block";
-animateCSS(renderer.domElement, "fadeIn");
+    update_profile();
+    animate();
+    renderer.domElement.style.display = "block";
+    animateCSS(renderer.domElement, "fadeIn");
 }
 
 function animate()
@@ -381,7 +544,7 @@ let leftEye = keypoints[1].position;
 let rightEye = keypoints[2].position;
 
 let nosex = (nose.x / videoWidth) * 1.0 - 0.5;
-let nosey = (nose.y / videoWidth) * 1.0 - 0.5;
+let nosey = -((nose.y / videoWidth) * 1.0 - 0.5);
 
 //let leftEyeX = (leftEye.x / videoWidth) * 1.0 - 0.5;
 //let rightEyeX = (rightEye.x / videoWidth) * 1.0 - 0.5;
@@ -412,17 +575,5 @@ startButton.onclick = function() {
     showVideo = false;
     startAnimation();
 }
-
-async function getNet()
-{
-    net = await posenet.load({
-        architecture: 'MobileNetV1',
-        outputStride: 16,
-        inputResolution: { width: videoWidth, height: videoHeight },
-        multiplier: 1.0
-    });
-}
-
-getNet();
 
 }, false);
